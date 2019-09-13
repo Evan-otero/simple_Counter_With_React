7c7b147bd1c3e2becedeb0e58cd053bcce8b7250
Evan Otero <Evan.otero19@gmail.com>
2019-09-13 19:00:39 +0000

    Working on counter

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -1,7 +1,26 @@
-import React from "react"
+import React from "react";
 
-export const Counter = () =>{
-    return(
-
-    )
+export const Counter = () => {
+	return (
+		<div className="row">
+			<div className="col-md-1">clock</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+			<div className="col-md-1">0</div>
+		</div>
+	);
 };
+let alpha = bravo => {
+	let time = 0;
+	setInterval(() => {
+		time += 1;
+		if (time == 10) time = 0;
+		return time;
+	}, bravo);
+};
+
+console.log(alpha(1000));
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Proptypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Counter } from "./Counter.js";
 
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
-			<div >
-                <Counter />
+			<div className="container">
+				<Counter />
 			</div>
 		);
 	}
